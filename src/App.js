13462fe9960709f94d21e6a783867bb465d16e15
shristi <gshristi40@gmail.com>
2023-07-13 21:18:02 +0530

    added buttons

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Login from './components/Login';
 import SignUp from './components/SignUp';
 
 
  const App = () => {
+  const [page,setPage] = useState(false)
   return (
-    <div className='cntnr'><Login/>
     
+    <div className='cntnr'>
+    <button onClick={()=>setPage(true)}>SignUp</button>
+    <button onClick={()=>setPage(false)} >Login</button>
+    {page ? <SignUp/> : <Login/>}
     </div>
   
   )
